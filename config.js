0b5bbbf83375f5923a9ec71eb1c918dc861dc52d
abracu <abracu@novation.com.co>
2018-01-07 22:00:09 -0500

    Creacion de Modelo User

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+/* Este archivo tiene la configuracion de el puerto y la conexion con la BD */
+
 module.exports = {
     port: process.env.PORT || 3001,
     db: process.env.MONGODB || 'mongodb://localhost:27017/shop'
